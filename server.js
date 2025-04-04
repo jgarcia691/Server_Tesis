e1fb5cbbaea1c19f25e977b751cd09f516f06995
jgarcia691 <108421222+jgarcia691@users.noreply.github.com>
2025-04-03 21:26:35 -0400

    agg ruta jurado

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 import tesisRoutes from "./src/tesis/routes.js"; 
 import routesEncargado from "./src/encargado/routes.js"; 
-import routesCarrera from "./src/carrera/routes.js"
+import routesCarrera from "./src/carrera/routes.js";
+import routesProfesor from "./src/profesor/routes.js";
+import routesJurado from "./src/jurado/routes.js";
 
 const app = express();
 
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use("/api", tesisRoutes); 
 app.use("/api/encargado", routesEncargado); 
 app.use("/api/carrera",routesCarrera);
+app.use("/api/profesor",routesProfesor);
+app.use("/api/jurado", routesJurado);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
