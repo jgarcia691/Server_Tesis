2208a0d4f598de926e084019ed246ce4090a74c0
jgarcia691 <jagarciar.info@gmail.com>
2025-08-01 20:53:53 -0400

    preparando despliegue

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 import { createClient } from "@libsql/client";
+import "dotenv/config"; // Importa dotenv para cargar las variables
 
 const db = createClient({
-  url: "libsql://service-jagarciar.aws-us-east-1.turso.io",
-  authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NDU3MTY2MjUsImlkIjoiYTg3ODkzNTAtOGYyOC00M2U4LWIxNmUtNmY0ODY0NTBhOTBmIiwicmlkIjoiNWE5ZjQ5YTAtNjJiZC00YTA4LWEyOWUtMDkxNTI1YjEzOTI3In0.uN_mDjQtN2MfiozmNGdZbdcdCkww-Mid4QBG8MhP7o8KlXsl6CKbWt5W4Yww87j-oRn9i3dAA34e6vJcRANvDg",
+  url: process.env.TURSO_DB_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
 export default db;
